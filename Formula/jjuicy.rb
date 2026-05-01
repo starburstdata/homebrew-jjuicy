afee98800165d5377aea34409e2e10cc2a5fa1cb
Lakshmikant Shrinivas <lakshmikant.shrinivas@starburstdata.com>
2026-05-01 00:36:40 -0400

    Add jjuicy formula

---
+++ Formula/jjuicy.rb
@@ -0,0 +1,19 @@
+class Jjuicy < Formula
+  desc "GUI for Jujutsu version control"
+  homepage "https://github.com/starburstdata/gg"
+  license "Apache-2.0"
+  head "https://github.com/starburstdata/gg.git", branch: "master"
+
+  depends_on "node" => :build
+  depends_on "rust" => :build
+
+  def install
+    system "npm", "install"
+    system "npm", "run", "build"
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/ju --version")
+  end
+end
