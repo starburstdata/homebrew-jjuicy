b6a434d12d3497b325a0172af7847db866e12c69
Lakshmikant Shrinivas <lakshmikant.shrinivas@starburstdata.com>
2026-05-01 11:29:01 -0400

    Update formula to v1.0.0

--- Formula/jjuicy.rb
+++ Formula/jjuicy.rb
@@ -2,7 +2,9 @@ class Jjuicy < Formula
   desc "GUI for Jujutsu version control"
   homepage "https://github.com/starburstdata/gg"
   license "Apache-2.0"
-  head "https://github.com/starburstdata/gg.git", branch: "pachu/rename"
+  url "https://github.com/starburstdata/jjuicy/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "89e6c6924092c9c11b92046d6ec02905aeaf2a5abc874e86b515767493549ec3"
+  version "1.0.0"
 
   depends_on "node" => :build
   depends_on "rust" => :build
