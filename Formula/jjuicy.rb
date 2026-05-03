a1e35410e29d3f78c9fc508ae56fc7e4708dbc8e
Lakshmikant Shrinivas <lakshmikant@gmail.com>
2026-05-03 13:25:49 -0400

    Bump version to 1.0.2-1

--- Formula/jjuicy.rb
+++ Formula/jjuicy.rb
@@ -2,9 +2,9 @@ class Jjuicy < Formula
   desc "GUI for Jujutsu version control"
   homepage "https://github.com/starburstdata/gg"
   license "Apache-2.0"
-  url "https://github.com/starburstdata/jjuicy/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "9126b61f721118254726c41af55d530e1e47efa61e8a82934deb153232964922"
-  version "1.0.2"
+  url "https://github.com/starburstdata/jjuicy/archive/refs/tags/v1.0.2-1.tar.gz"
+  sha256 "a22e903a9c08460eb4fec3e267130d1a2af88c1cb2062bb596cf3b97076886ee"
+  version "1.0.2-1"
 
   depends_on "node" => :build
   depends_on "rust" => :build
